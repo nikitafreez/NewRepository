b176c35562d6d0b20e7100304c2a40540898aecb
nikitafreez <nikita2002393@gmail.com>
2021-03-04 14:53:08 +0300

    &*Super Calculator*&

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("Результат: ", i * 2);
+            Console.ReadKey();
+        }
+    }
+}
